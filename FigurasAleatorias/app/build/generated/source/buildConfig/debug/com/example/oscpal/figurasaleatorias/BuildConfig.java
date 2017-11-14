c34522b597b5ebf95dcf7bc268eddf27bb541f31
oscarcillo18 <oscar.gimenez1997@gmail.com>
2017-11-14 10:42:24 +0100

    figuras todo

---
+++ FigurasAleatorias/app/build/generated/source/buildConfig/debug/com/example/oscpal/figurasaleatorias/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.oscpal.figurasaleatorias;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.oscpal.figurasaleatorias";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
